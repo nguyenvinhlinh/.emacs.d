6e2548ebce98f86eaf5502dd1b1c47d5708722c7
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-16 16:17:38 +0700

    add keybinding to hideshow mode, C-c left right up down

--- config/linh-hideshow.el
+++ config/linh-hideshow.el
@@ -1,4 +1,10 @@
-(add-hook 'ruby-mode-hook 'hs-minor-mode)
+(add-hook 'ruby-mode-hook
+          (lambda()
+            (local-set-key (kbd "C-c <right>") 'hs-show-block)
+            (local-set-key (kbd "C-c <left>") 'hs-hide-block)
+            (local-set-key (kbd "C-c <up>") 'hs-hide-all)
+            (local-set-key (kbd "C-c <down>") 'hs-show-all)
+            (hs-minor-mode t)))
 (add-to-list 'hs-special-modes-alist
              `(ruby-mode
                ,(rx (or "def" "class" "module" "{" "["))
