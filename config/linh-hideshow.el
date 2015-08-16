1f6d60c91a364bc9cc711634a8c8ab43c0358f66
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-16 16:24:22 +0700

    a bit modify

--- config/linh-hideshow.el
+++ config/linh-hideshow.el
@@ -3,13 +3,15 @@
 ;; https://chrisbarrettnz.wordpress.com/2013/06/15/ruby-code-folding-with-emacs/
 ;; keybinding:
 ;; http://emacs-fu.blogspot.com/2008/12/showing-and-hiding-blocks-of-code.html
-(add-hook 'ruby-mode-hook
-          (lambda()
+(add-hook 'ruby-mode-hook 'hs-minor-mode)
+(add-hook 'hs-minor-mode-hook
+          (lambda ()
             (local-set-key (kbd "C-c <right>") 'hs-show-block)
             (local-set-key (kbd "C-c <left>") 'hs-hide-block)
             (local-set-key (kbd "C-c <up>") 'hs-hide-all)
             (local-set-key (kbd "C-c <down>") 'hs-show-all)
-            (hs-minor-mode t)))
+            ))
+
 (add-to-list 'hs-special-modes-alist
              `(ruby-mode
                ,(rx (or "def" "class" "module" "{" "["))
