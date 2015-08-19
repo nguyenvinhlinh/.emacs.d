e2a036439178e61e06131db81965ab66ed36f471
Nguyen Vinh Linh <linhnv@cogini.com>
2015-08-19 21:17:06 +0700

    add path of sqlplus

--- config/linh-sqli.el
+++ config/linh-sqli.el
@@ -2,5 +2,5 @@
         (let ((current (getenv "LD_LIBRARY_PATH"))
               (new "/opt/oracle/instantclient_12_1"))
           (if current (concat new ":" current) new)))
-
+(setq sqlplus-command "/opt/oracle/instantclient_12_1/sqlplus")
 (provide 'linh-sqli)
