37da7adbd512bfb5507f0a1f5d45c0d0a28e2ee5
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-09-08 13:13:59 +0700

    a bit mod

--- config/linh-indentation.el
+++ config/linh-indentation.el
@@ -1,7 +1,4 @@
 (setq-default indent-tabs-mode nil)
 (setq css-indent-offset 2)
 (setq js-indent-level 2)
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
 (provide 'linh-indentation)
