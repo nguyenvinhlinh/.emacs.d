f77d69de97a011d19f8e8bc61d584012998ba21f
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-12-13 22:42:55 +0700

    “add elixir mode, yaml mode

--- config/linh-web-mode.el
+++ config/linh-web-mode.el
@@ -8,6 +8,7 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.html.eex" . web-mode))
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
