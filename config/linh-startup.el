f87186271b55d9b5e19b4e9f2048faf4674c59e2
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2016-01-22 18:33:25 +0700

    disable menubar, disable toolbar

--- config/linh-startup.el
+++ config/linh-startup.el
@@ -3,5 +3,7 @@
 (setq-default tab-width 2)
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
+(menu-bar-mode -1)
+(tool-bar-mode -1)
 (delete-selection-mode)
 (provide 'linh-startup)
