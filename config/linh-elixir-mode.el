8b1894e5b6ffcc72dbe515efbfa4e1c6a4743771
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2016-01-22 12:04:39 +0700

    update starter  kit, update elixir mode

---
+++ config/linh-elixir-mode.el
@@ -0,0 +1,8 @@
+(require 'alchemist)
+
+(setq alchemist-mix-command      "/home/nguyenvinhlinh/.kiex/elixirs/elixir-1.2.1/bin/mix")
+(setq alchemist-iex-program-name "/home/nguyenvinhlinh/.kiex/elixirs/elixir-1.2.1/bin/iex")
+(setq alchemist-execute-command  "/home/nguyenvinhlinh/.kiex/elixirs/elixir-1.2.1/bin/elixir")
+(setq alchemist-compile-command  "/home/nguyenvinhlinh/.kiex/elixirs/elixir-1.2.1/bin/elixirc")
+
+(provide 'linh-elixir-mode)
